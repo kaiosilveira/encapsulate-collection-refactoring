6dda53ff29fd8a755778f6d17d3c310e898ff07f
Kaio Silveira <silveira.kaio@icloud.com>
2023-03-22 14:21:13 +0000

    update client to use `Person.addCourses`
    This is to avoid accessing person.courses directly

--- src/clients/writer2/index.js
+++ src/clients/writer2/index.js
@@ -2,6 +2,6 @@ import Course from '../../entities/Course.js';
 
 export function appendBasicCoursesToPersonByCourseName(aPerson, courses) {
   for (const name of courses) {
-    aPerson.courses.push(new Course(name, false));
+    aPerson.addCourse(new Course(name, false));
   }
 }
